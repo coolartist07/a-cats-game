4ab6258cfc74cb8d42d1b82dfa3197fcdae1e5a9
lauren ⋆｡°✩ <laurengalaxy38@gmail.com>
2026-01-09 17:42:14 -0800

    big update #3
    -imported and changed cat2 animation
    -made loading cat animation sprite
    -made loading screen
    -connected main menu, loading screen, and game together

---
+++ loading_screen.gd
@@ -0,0 +1,17 @@
+extends Control
+
+var progress = []
+var scene_name
+var scene_load_status = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	scene_name = "res://game.tscn"
+	ResourceLoader.load_threaded_request(scene_name)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	scene_load_status = ResourceLoader.load_threaded_get_status(scene_name, progress)
+	if scene_load_status == ResourceLoader.THREAD_LOAD_LOADED:
+		var new_scene = ResourceLoader.load_threaded_get(scene_name)
+		get_tree().change_scene_to_packed(new_scene)
--- main_menu.gd
+++ main_menu.gd
@@ -12,7 +12,8 @@ func _process(delta: float) -> void:
 
 # game starts when button is pressed
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://game.tscn")
+	var loading_screen = load("res://loading_screen.tscn")
+	get_tree().change_scene_to_packed(loading_screen)
 
 # quits game when button is pressed
 func _on_quit_button_pressed() -> void:
