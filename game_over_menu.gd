d1e079e5374d64be78a97e37f5058307e6059dc2
lauren ⋆｡°✩ <laurengalaxy38@gmail.com>
2026-01-11 00:16:02 -0800

    readjusted game over menu + added home button

--- game_over_menu.gd
+++ game_over_menu.gd
@@ -10,3 +10,9 @@ func _on_restart_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_home_button_pressed() -> void:
+	# unpause so main can be interacted with
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://main.tscn")
